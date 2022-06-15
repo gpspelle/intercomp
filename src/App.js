977ce05ff8d900e42e67a10f40df4893cc71bb7f
gpspelle <gpsunicamp016@gmail.com>
2022-06-15 15:41:41 -0300

    background and avoid scroll

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import Tiles from './components/Tiles';
+
+import "./App.css"
 import "./scss/Tiles.scss"
+
+document.body.style.overflow = "hidden"
 
 function App() {
   return (
     <div>
+      <div className="hide">
+        <h1 id="versatility">CAMPEÃO INTERCOMP 2022</h1>
+      </div>
+      <div className="hide">
+        <h1 id="versatility">CAMPEÃO INTERCOMP 2022</h1>
+      </div>
+      <div className="hide">
+        <h1 id="versatility">CAMPEÃO INTERCOMP 2022</h1>
+      </div>
+      <div className="hide">
+        <h1 id="versatility">CAMPEÃO INTERCOMP 2022</h1>
+      </div>
+      <div className="hide">
+        <h1 id="versatility">CAMPEÃO INTERCOMP 2022</h1>
+      </div>
+      <div className="hide">
+        <h1 id="versatility">CAMPEÃO INTERCOMP 2022</h1>
+      </div>
+      <div className="hide">
+        <h1 id="versatility">CAMPEÃO INTERCOMP 2022</h1>
+      </div>
       <Tiles />
     </div>
     );
