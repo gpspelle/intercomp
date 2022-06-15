1902bdaf507f492434d8bc1d959e9995debe6bae
gpspelle <gpsunicamp016@gmail.com>
2022-06-15 15:20:10 -0300

    add logo for every competitor

--- src/components/Tiles.js
+++ src/components/Tiles.js
@@ -6,17 +6,12 @@ const range = (start, end, length = end - start + 1) =>
   Array.from({ length }, (_, i) => start + i)
 
 const numberOfCompetitors = COMPETITORS.length
-const competitorNumberToCompetitorName = {
-    1: 'UNICAMP',
-    2: 'UFU',
-    3: 'EACH'
-}
 
 export default function Tiles () {
     return (
         <div className="wrap">
             {range(0, 119).map((i) => {
-                return <div key={i} className={`${competitorNumberToCompetitorName[i % numberOfCompetitors]}`} ></div>
+                return <div key={i} className={`${COMPETITORS[i % numberOfCompetitors]}`} ></div>
             })}
         </div>
     )
