1800f0a8b057b58c9bf2f3851c59fd34c369a48e
gpspelle <gpsunicamp016@gmail.com>
2022-06-15 22:07:07 -0300

    improved texts

--- src/App.js
+++ src/App.js
@@ -15,15 +15,27 @@ function App() {
   useEffect(() => {
     setTimeout(() => {
       setState(1)
-    }, 5000)
+    }, 10000)
   }, [])
 
   return (
     <div>
-      <div className="hide">
-        <h1 id="versatility">CAMPEÃO INTERCOMP 2022</h1>
-      </div>
-      {state === 0 ? <Tiles /> : <Champion />}
+      {state === 0 ?
+        <>
+          <h2>QUEM VAI SER CAMPEÃO DO INTERCOMP 2022?</h2>
+          <Tiles />
+          <div style={{ position: "absolute", bottom: "10px" }}>
+            <h4>Aguarde os nossos algoritmos calcularem o resultado...</h4>
+          </div>
+        </>
+        :
+        <>
+          <h2>UNICAMP PE PE PE Ô</h2>
+          <h2>UNICAMP PE PE PE Ô</h2>
+          <h2>UNICAMP PE PE PE Ô</h2>
+          <Champion />
+        </>
+      }
     </div>
     );
 }
